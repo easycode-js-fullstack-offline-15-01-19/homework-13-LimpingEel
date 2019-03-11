d6f431834b4005bb8e353dd50bb91f8e0ceb19a6
LimpingEel <nik.doroshenko.00@ukr.net>
2019-03-11 22:49:14 +0200

    added task

--- homework.js
+++ homework.js
@@ -1,2 +1,18 @@
 // -------------------------- Home work --------------------------
-// -------------------------- ФИО --------------------------
+// -------------------------- Дорошенко Никита Андреевич --------------------------
+
+
+;(function ($) {
+	const menuName = $('.menu-name');
+	const menuList = $('.menu-list');
+
+	menuName.on('mouseover', function(e) {
+		menuList.removeClass('active');
+		menuName.removeClass('active');
+		$(this).parent('.menu').find('ul').addClass('active');
+		menuList.removeClass('menu-name-after');
+		menuName.removeClass('menu-name-after');
+		$(this).addClass('menu-name-after')
+	});
+
+}(jQuery));
